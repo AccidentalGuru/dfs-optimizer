b26aebe0e67c12aa6d7fd16b610a1a3e87b7b121
Jose Rojas <jmrojas2332@gmail.com>
2018-07-23 22:29:35 -0500

    Separate backend into blueprints. Introduce unit testing

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,26 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
-app = Flask(__name__)#, static_folder="static", template_folder="templates)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy()
+migrate = Migrate()
 
-from app import models, routes
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
+
+from app import models
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,10 +1,10 @@
-from flask import jsonify, make_response, render_template, request, session
+from flask import jsonify, make_response, request
 from sqlalchemy import or_
-from app import app, db
-from app.models import User
+from app import db
+from app.auth import bp
 
 
-@app.route('/api/login', methods=['POST'])
+@bp.route('/login', methods=['POST'])
 def login():
     post_data = request.get_json()
 
@@ -39,7 +39,7 @@ def login():
         return make_response(jsonify(response_object)), 500
 
 
-@app.route('/api/logout', methods=['POST'])
+@bp.route('/logout', methods=['POST'])
 def logout():
     auth_header = request.headers.get('Authorization')
     if auth_header:
@@ -84,7 +84,7 @@ def logout():
         return make_response(jsonify(responseObject)), 403
 
 
-@app.route('/api/register', methods=['POST'])
+@bp.route('/register', methods=['POST'])
 def register():
     post_data = request.get_json()
     user = User.query.filter(or_(User.username==post_data.get('username'), User.email==post_data.get('email'))).first()
@@ -122,58 +122,3 @@ def register():
         }
 
         return make_response(jsonify(response_object)), 202
-
-
-@app.route('/api/user', methods=['GET'])
-def user():
-    auth_header = request.headers.get('Authorization')
-    if auth_header:
-        try:
-            auth_token = auth_header.split(' ')[1]
-        except IndexError:
-            response_object = {
-                'status': 'fail',
-                'message': 'Bearer token malformed.'
-            }
-
-            return make_response(jsonify(response_object)), 401
-    else:
-        auth_token = ''
-
-    if auth_token:
-        resp = User.decode_auth_token(auth_token)
-
-        if not isinstance(resp, str):
-            user = User.query.filter_by(id=resp).first()
-            response_object = {
-                'status': 'success',
-                'data': {
-                    'user_id': user.id,
-                    'username': user.username,
-                    'email': user.email,
-                    'registered_on': user.registered_on
-                }
-            }
-
-            return make_response(jsonify(response_object)), 200
-
-        response_object = {
-            'status': 'fail',
-            'message': resp
-        }
-
-        return make_response(jsonify(response_object)), 401
-    else:
-        responseObject = {
-            'status': 'fail',
-            'message': 'Provide a valid auth token.'
-        }
-
-        return make_response(jsonify(responseObject)), 401
-
-
-# Routes should be added above this so they take precedence over catch all
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return render_template('index.html')
