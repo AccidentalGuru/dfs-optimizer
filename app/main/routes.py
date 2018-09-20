6fb77b64ca8b57acc2589e11588ffc37562afc8f
jmrojas2332 <jmrojas2332@gmail.com>
2018-09-19 23:18:50 -0500

    Migration update

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,4 +5,5 @@ from app.main import bp
 @bp.route('/', defaults={'path': ''})
 @bp.route('/<path:path>')
 def catch_all(path):
+    # catch_all catches all non-api routes so routing can be done on frontend
     return render_template('index.html')
