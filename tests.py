c7ccbbc1c924f627a1a62de311a0b8a31629d17e
jmrojas2332 <jmrojas2332@gmail.com>
2018-09-26 15:31:54 -0500

    Fix out of context error

--- tests.py
+++ tests.py
@@ -9,7 +9,20 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 
-class TestUserModel(unittest.TestCase):
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+
+class TestUserModel(BaseTestCase):
     def test_encode_auth_token(self):
         user = User(username='test', email='test@test.com', password='test')
         db.session.add(user)
