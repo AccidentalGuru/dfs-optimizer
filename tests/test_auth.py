5f245b2b7b75f0e921537ee39efe198bda2e9740
jmrojas2332 <jmrojas2332@gmail.com>
2018-10-03 23:21:15 -0500

    Refactor unit tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -6,14 +6,48 @@ from app.models import BlacklistToken, User
 from base import BaseTestCase
 
 
+def register_user(self, username, email, password):
+    return self.client.post(
+        '/api/register',
+        data=json.dumps(dict(
+            username=username,
+            email=email,
+            password=password)),
+        content_type='application/json'
+    )
+
+
+def login_user(self, username, password):
+    return self.client.post(
+        '/api/login',
+        data=json.dumps(dict(
+            username=username,
+            password=password)),
+        content_type='application/json'
+    )
+
+
+def logout_user(self, token):
+    return self.client.post(
+        '/api/logout',
+        headers=dict(
+            Authorization='Bearer ' + token
+        )
+    )
+
+
+def get_user_status(self, token):
+    return self.client.get(
+        '/api/status',
+        headers=dict(
+            Authorization='Bearer ' + token
+        )
+    )
+
+
 class TestAuthBluePrint(BaseTestCase):
     def test_registration(self):
-        response = self.client.post(
-            '/api/register',
-            data=json.dumps(dict(username='test', email='test@test.com', password='test')),
-            content_type='application/json'
-        )
-
+        response = register_user(self, 'test', email='test@test.com', password='test')
         data = json.loads(response.data.decode())
         self.assertTrue(data['status'] == 'success')
         self.assertTrue(data['message'] == 'Successfully registered.')
@@ -30,11 +64,7 @@ class TestAuthBluePrint(BaseTestCase):
         db.session.add(user)
         db.session.commit()
 
-        response = self.client.post(
-            '/api/register',
-            data=json.dumps(dict(username='test', email='test@test.com', password='test')),
-            content_type='application/json'
-        )
+        response = register_user(self, 'test', 'test@test.com', 'test')
         data = json.loads(response.data.decode())
         self.assertTrue(data['status'] == 'fail')
         self.assertTrue(data['message'] == 'User already exists. Please Log in.')
@@ -42,12 +72,7 @@ class TestAuthBluePrint(BaseTestCase):
         self.assertEqual(response.status_code, 202)
 
     def test_login_with_registered_user(self):
-        resp_register = self.client.post(
-            '/api/register',
-            data=json.dumps(dict(username='test', email='test@test.com', password='test')),
-            content_type='application/json'
-        )
-
+        resp_register = register_user(self, 'test', 'test@test.com', 'test')
         data_register = json.loads(resp_register.data.decode())
         self.assertTrue(data_register['status'] == 'success')
         self.assertTrue(data_register['message'] == 'Successfully registered.')
@@ -55,12 +80,7 @@ class TestAuthBluePrint(BaseTestCase):
         self.assertTrue(resp_register.content_type == 'application/json')
         self.assertEqual(resp_register.status_code, 201)
 
-        response = self.client.post(
-            '/api/login',
-            data=json.dumps(dict(username='test', password='test')),
-            content_type='application/json'
-        )
-
+        response = login_user(self, 'test', 'test')
         data = json.loads(response.data.decode())
         self.assertTrue(data['status'] == 'success')
         self.assertTrue(data['message'] == 'Successfully logged in.')
@@ -69,12 +89,7 @@ class TestAuthBluePrint(BaseTestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_login_non_registered_user(self):
-        response = self.client.post(
-            '/api/login',
-            data=json.dumps(dict(username='test', password='test')),
-            content_type='application/json'
-        )
-
+        response = login_user(self, 'test', 'test')
         data = json.loads(response.data.decode())
         self.assertTrue(data['status'] == 'fail')
         self.assertTrue(data['message'] == 'User does not exist.')
@@ -82,19 +97,8 @@ class TestAuthBluePrint(BaseTestCase):
         self.assertEqual(response.status_code, 404)
 
     def test_user_status(self):
-        resp_register = self.client.post(
-            '/api/register',
-            data=json.dumps(dict(username='test', email='test@test.com', password='test')),
-            content_type='application/json'
-        )
-
-        response = self.client.get(
-            '/api/status',
-            headers=dict(
-                Authorization='Bearer ' + json.loads(resp_register.data.decode())['auth_token']
-            )
-        )
-
+        resp_register = register_user(self, 'test', 'test@test.com', 'test')
+        response = get_user_status(self, json.loads(resp_register.data.decode())['auth_token'])
         data = json.loads(response.data.decode())
         self.assertTrue(data['status'] == 'success')
         self.assertTrue(data['data'] is not None)
@@ -103,12 +107,7 @@ class TestAuthBluePrint(BaseTestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_valid_logout(self):
-        resp_register = self.client.post(
-            '/api/register',
-            data=json.dumps(dict(username='test', email='test@test.com', password='test')),
-            content_type='application/json'
-        )
-
+        resp_register = register_user(self, 'test', 'test@test.com', 'test')
         data_register = json.loads(resp_register.data.decode())
         self.assertTrue(data_register['status'] == 'success')
         self.assertTrue(data_register['message'] == 'Successfully registered.')
@@ -116,12 +115,7 @@ class TestAuthBluePrint(BaseTestCase):
         self.assertTrue(resp_register.content_type == 'application/json')
         self.assertEqual(resp_register.status_code, 201)
 
-        resp_login = self.client.post(
-            '/api/login',
-            data=json.dumps(dict(username='test', password='test')),
-            content_type='application/json'
-        )
-
+        resp_login = login_user(self, 'test', 'test')
         data_login = json.loads(resp_login.data.decode())
         self.assertTrue(data_login['status'] == 'success')
         self.assertTrue(data_login['message'] == 'Successfully logged in.')
@@ -129,25 +123,14 @@ class TestAuthBluePrint(BaseTestCase):
         self.assertTrue(resp_login.content_type == 'application/json')
         self.assertEqual(resp_login.status_code, 200)
 
-        response = self.client.post(
-            '/api/logout',
-            headers=dict(
-                Authorization='Bearer ' + json.loads(resp_register.data.decode())['auth_token']
-            )
-        )
-
+        response = logout_user(self, json.loads(resp_register.data.decode())['auth_token'])
         data = json.loads(response.data.decode())
         self.assertTrue(data['status'] == 'success')
         self.assertTrue(data['message'] == 'Successfully logged out.')
         self.assertEqual(response.status_code, 200)
 
     def test_invalid_logout(self):
-        resp_register = self.client.post(
-            '/api/register',
-            data=json.dumps(dict(username='test', email='test@test.com', password='test')),
-            content_type='application/json'
-        )
-
+        resp_register = register_user(self, 'test', 'test@test.com', 'test')
         data_register = json.loads(resp_register.data.decode())
         self.assertTrue(data_register['status'] == 'success')
         self.assertTrue(data_register['message'] == 'Successfully registered.')
@@ -155,43 +138,23 @@ class TestAuthBluePrint(BaseTestCase):
         self.assertTrue(resp_register.content_type == 'application/json')
         self.assertEqual(resp_register.status_code, 201)
 
-        resp_login = self.client.post(
-            '/api/login',
-            data=json.dumps(dict(username='test', password='test')),
-            content_type='application/json'
-        )
-
+        resp_login = login_user(self, 'test', 'test')
         data_login = json.loads(resp_login.data.decode())
         self.assertTrue(data_login['status'] == 'success')
         self.assertTrue(data_login['message'] == 'Successfully logged in.')
         self.assertTrue(data_login['auth_token'])
         self.assertTrue(resp_login.content_type == 'application/json')
         self.assertEqual(resp_login.status_code, 200)
-
         time.sleep(6)
-        response = self.client.post(
-            '/api/logout',
-            headers=dict(
-                Authorization='Bearer ' + json.loads(resp_register.data.decode())['auth_token']
-            )
-        )
 
+        response = logout_user(self, json.loads(resp_register.data.decode())['auth_token'])
         data = json.loads(response.data.decode())
         self.assertTrue(data['status'] == 'fail')
         self.assertTrue(data['message'] == 'Signature expired. Please log in again.')
         self.assertEqual(response.status_code, 401)
 
     def test_valid_blacklisted_token_logout(self):
-        resp_register = self.client.post(
-            '/api/register',
-            data=json.dumps(dict(
-                username='test',
-                email='test@test.com',
-                password='test'
-            )),
-            content_type='application/json',
-        )
-
+        resp_register = register_user(self, 'test', 'test@test.com', 'test')
         data_register = json.loads(resp_register.data.decode())
         self.assertTrue(data_register['status'] == 'success')
         self.assertTrue(data_register['message'] == 'Successfully registered.')
@@ -199,12 +162,7 @@ class TestAuthBluePrint(BaseTestCase):
         self.assertTrue(resp_register.content_type == 'application/json')
         self.assertEqual(resp_register.status_code, 201)
 
-        resp_login = self.client.post(
-            '/api/login',
-            data=json.dumps(dict(username='test', password='test')),
-            content_type='application/json'
-        )
-
+        resp_login = login_user(self, 'test', 'test')
         data_login = json.loads(resp_login.data.decode())
         self.assertTrue(data_login['status'] == 'success')
         self.assertTrue(data_login['message'] == 'Successfully logged in.')
@@ -217,40 +175,20 @@ class TestAuthBluePrint(BaseTestCase):
         db.session.add(blacklist_token)
         db.session.commit()
 
-        response = self.client.post(
-            '/api/logout',
-            headers=dict(
-                Authorization='Bearer ' + json.loads(resp_login.data.decode())['auth_token']
-            )
-        )
-
+        response = logout_user(self, json.loads(resp_login.data.decode())['auth_token'])
         data = json.loads(response.data.decode())
         self.assertTrue(data['status'] == 'fail')
         self.assertTrue(data['message'] == 'Token blacklisted. Please log in again.')
         self.assertEqual(response.status_code, 401)
 
     def test_valid_blacklisted_token_user(self):
-        resp_register = self.client.post(
-            '/api/register',
-            data=json.dumps(dict(
-                username='test',
-                email='test@test.com',
-                password='test'
-            )),
-            content_type='application/json',
-        )
-
+        resp_register = register_user(self, 'test', 'test@test.com', 'test')
         blacklist_token = BlacklistToken(
             token=json.loads(resp_register.data.decode())['auth_token'])
         db.session.add(blacklist_token)
         db.session.commit()
 
-        response = self.client.get(
-            '/api/status',
-            headers=dict(
-                Authorization='Bearer ' + json.loads(resp_register.data.decode())['auth_token']
-            )
-        )
+        response = get_user_status(self, json.loads(resp_register.data.decode())['auth_token'])
         data = json.loads(response.data.decode())
         self.assertTrue(data['status'] == 'fail')
         self.assertTrue(data['message'] == 'Token blacklisted. Please log in again.')
