0dfeead9a686b59bb08302374466e5cf0c149aa4
jmrojas2332 <jmrojas2332@gmail.com>
2018-11-11 06:33:17 -0600

    remove test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -129,29 +129,30 @@ class TestAuthBluePrint(BaseTestCase):
         self.assertTrue(data['message'] == 'Successfully logged out.')
         self.assertEqual(response.status_code, 200)
 
-    def test_invalid_logout(self):
-        resp_register = register_user(self, 'test', 'test@test.com', 'test')
-        data_register = json.loads(resp_register.data.decode())
-        self.assertTrue(data_register['status'] == 'success')
-        self.assertTrue(data_register['message'] == 'Successfully registered.')
-        self.assertTrue(data_register['auth_token'])
-        self.assertTrue(resp_register.content_type == 'application/json')
-        self.assertEqual(resp_register.status_code, 201)
-
-        resp_login = login_user(self, 'test', 'test')
-        data_login = json.loads(resp_login.data.decode())
-        self.assertTrue(data_login['status'] == 'success')
-        self.assertTrue(data_login['message'] == 'Successfully logged in.')
-        self.assertTrue(data_login['auth_token'])
-        self.assertTrue(resp_login.content_type == 'application/json')
-        self.assertEqual(resp_login.status_code, 200)
-        time.sleep(6)
-
-        response = logout_user(self, json.loads(resp_register.data.decode())['auth_token'])
-        data = json.loads(response.data.decode())
-        self.assertTrue(data['status'] == 'fail')
-        self.assertTrue(data['message'] == 'Signature expired. Please log in again.')
-        self.assertEqual(response.status_code, 401)
+    # Note: Change token expiration in models.py to 5 seconds if you want to run this test
+    # def test_invalid_logout(self):
+    #     resp_register = register_user(self, 'test', 'test@test.com', 'test')
+    #     data_register = json.loads(resp_register.data.decode())
+    #     self.assertTrue(data_register['status'] == 'success')
+    #     self.assertTrue(data_register['message'] == 'Successfully registered.')
+    #     self.assertTrue(data_register['auth_token'])
+    #     self.assertTrue(resp_register.content_type == 'application/json')
+    #     self.assertEqual(resp_register.status_code, 201)
+    #
+    #     resp_login = login_user(self, 'test', 'test')
+    #     data_login = json.loads(resp_login.data.decode())
+    #     self.assertTrue(data_login['status'] == 'success')
+    #     self.assertTrue(data_login['message'] == 'Successfully logged in.')
+    #     self.assertTrue(data_login['auth_token'])
+    #     self.assertTrue(resp_login.content_type == 'application/json')
+    #     self.assertEqual(resp_login.status_code, 200)
+    #     time.sleep(6)
+    #
+    #     response = logout_user(self, json.loads(resp_register.data.decode())['auth_token'])
+    #     data = json.loads(response.data.decode())
+    #     self.assertTrue(data['status'] == 'fail')
+    #     self.assertTrue(data['message'] == 'Signature expired. Please log in again.')
+    #     self.assertEqual(response.status_code, 401)
 
     def test_valid_blacklisted_token_logout(self):
         resp_register = register_user(self, 'test', 'test@test.com', 'test')
